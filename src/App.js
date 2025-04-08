ae4a7e5f8c4c1ca9645fa7ebc195940171f9645d
gyuminwoo <ug034516@gmail.com>
2025-04-08 19:14:10 +0900

    React UI 구현 - 게시글 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import{BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+import PostDetail from "./PostDetail";
+import PostEdit from "./PostEdit";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+        <div className="App">
+            <Routes>
+                <Route path={"/"} element={<Home/>} />
+                <Route path={"/create"} element={<CreatePost/>} />
+                <Route path={"/post/:id"} element={<PostDetail/>} />
+                <Route path={"/post/edit/:id"} element={<PostEdit/>} />
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
