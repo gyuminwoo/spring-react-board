6bdef2243eb0ce7c7d08439864f8a4c60521e089
gyuminwoo <ug034516@gmail.com>
2025-04-09 23:49:01 +0900

    React와 Spring Boot 연동을 위한 환경 설정

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,7 +1,31 @@
 import "./css/create.css";
+import {useState} from "react";
+import axios from "axios";
 
 // 投稿作成ページコンポーネント
 let CreatePost = ()=>{
+
+    let [formData, setFormData] = useState({
+        title: '',
+        content: ''
+    });
+
+    let onChangeFormData = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        });
+    };
+
+    const onClickSubmit = () => {
+        axios.post(`${process.env.REACT_APP_API_URL}/posts`, formData).then(res => {
+            console.log(res.data);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+
     return (
         <div className={"create-post-container"}>
             <h1 className={"create-post-title"}>게시글 작성</h1>
@@ -10,15 +34,15 @@ let CreatePost = ()=>{
               　{/* タイトル入力欄 */}
                 <div className={"form-group"}>
                     <label htmlFor={"title"}>제목</label>
-                    <input id={"title"} type={"text"} placeholder={"제목을 입력하세요."}/>
+                    <input id={"title"} type={"text"} name={"title"} onChange={onChangeFormData} placeholder={"제목을 입력하세요."}/>
                 </div>
                 {/* コンテンツ入力欄 */}
                 <div className={"form-group"}>
                     <label htmlFor={"content"}>내용</label>
-                    <textarea id={"content"} placeholder={"내용을 입력하세요."}/>
+                    <textarea id={"content"} name={"content"} onChange={onChangeFormData} placeholder={"내용을 입력하세요."}/>
                 </div>
                 {/* 登録ボタン */}
-                <button type={"submit"} className={"submit-button"}>등록</button>
+                <button type={"submit"} className={"submit-button"} onClick={onClickSubmit}>등록</button>
             </form>
         </div>
     );
