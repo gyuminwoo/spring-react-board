b34c3bff1854078c2961fdb989d48a8cdc85ad6b
gyuminwoo <ug034516@gmail.com>
2025-04-11 18:41:43 +0900

    엔티티 생성

--- src/main/java/restapi/prac/model/Comment.java
+++ src/main/java/restapi/prac/model/Comment.java
@@ -9,36 +9,27 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class Post {
-
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
-    private String title;
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post; // コメント対象の投稿
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user; // 投稿者表示のため
+    private User user; // コメント投稿者
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
     }
 }
