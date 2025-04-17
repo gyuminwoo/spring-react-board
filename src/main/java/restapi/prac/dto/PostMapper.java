c761c1108c75734eb704534cfdeff67cef62eb91
본가PC <ug034516@gmail.com>
2025-04-17 16:55:14 +0900

    게시글 관련 DTO 생성

--- src/main/java/restapi/prac/dto/PostMapper.java
+++ src/main/java/restapi/prac/dto/PostMapper.java
@@ -1,7 +1,9 @@
 package restapi.prac.dto;
 
+import restapi.prac.dto.request.PostRequestDto;
 import restapi.prac.dto.response.PostResponseDto;
 import restapi.prac.model.Post;
+import restapi.prac.model.User;
 
 public class PostMapper {
     public static PostResponseDto toDto(Post post) {
@@ -12,6 +14,14 @@ public class PostMapper {
                 .authorName(post.getUser().getName())
                 .createdAt(post.getCreatedAt())
                 .updatedAt(post.getUpdatedAt())
+                .build();
+    }
+
+    public static Post toEntity(PostRequestDto dto, User user) {
+        return Post.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .user(user)
                 .build();
     }
 }
--- src/main/java/restapi/prac/dto/response/PostResponseDto.java
+++ src/main/java/restapi/prac/dto/response/PostResponseDto.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 public class PostResponseDto {
     private Long id;
     private String title;
